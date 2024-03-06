8c61be676dae1b61ab6478d6f9f844a6be6ba461
menisadi <menisadi@gmail.com>
2024-03-06 09:11:18 +0200

    Moved files, rename main file, move ifmain code to main function
    The main function got renamed to convert. Also added some config info

--- src/pgn2anki/__main__.py
+++ src/pgn2anki/__main__.py
@@ -9,6 +9,7 @@ import io
 import numpy as np
 import argparse
 
+
 def parseArguments():
     # Create argument parser
     parser = argparse.ArgumentParser()
@@ -20,7 +21,7 @@ def parseArguments():
     parser.add_argument("-C", "--color", help="Pick a color.", type=str, default="")
 
     # Print version
-    parser.add_argument("--version", action="version", version='%(prog)s - Version 0.2')
+    parser.add_argument("--version", action="version", version="%(prog)s - Version 0.2")
 
     # Parse arguments
     args = parser.parse_args()
@@ -41,26 +42,28 @@ def pgn_travel(node):
             yield [node.board(), child.board(), node.ply(), colors(node.turn())]
             yield from pgn_travel(child)
 
-            
+
 def fetch_pgn(url):
-    
+
     # # The website server expected a header to be passed:
     # So we give him this
     # Taken from https://stackoverflow.com/questions/60837734/you-dont-have-permission-to-access-this-resource-python-webscraping
-    headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) '\
-            'AppleWebKit/537.36 (KHTML, like Gecko) '\
-            'Chrome/75.0.3770.80 Safari/537.36'}
+    headers = {
+        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) "
+        "AppleWebKit/537.36 (KHTML, like Gecko) "
+        "Chrome/75.0.3770.80 Safari/537.36"
+    }
 
     req = requests.get(url, headers=headers)
     soup = BeautifulSoup(req.text, "html.parser")
 
     # For some reason the id of the div containing the PGN is named olga-data
-    olga = soup.find(id = "olga-data")
+    olga = soup.find(id="olga-data")
     # print(olga.attrs['pgn'])
-    return olga.attrs['pgn']
+    return olga.attrs["pgn"]
 
-def main(source_pgn, color_pick):
 
+def convert(source_pgn, color_pick):
     pgn = chess.pgn.read_game(source_pgn)
     # board = pgn.board()
     name = pgn.headers["White"] + " vs " + pgn.headers["Black"]
@@ -74,22 +77,22 @@ def main(source_pgn, color_pick):
     # Define the model
     anki_model = genanki.Model(
         model_id,
-        'Chess PGN',
+        "Chess PGN",
         fields=[
-            {'name': 'Before'},
-            {'name': 'After'},
-            {'name': 'Move Number'},
-            {'name': 'Turn'},
+            {"name": "Before"},
+            {"name": "After"},
+            {"name": "Move Number"},
+            {"name": "Turn"},
         ],
         templates=[
             {
-                'name': 'Card 1',
-                'qfmt': '<div style="text-align: center; font-weight: bold;">{{Move Number}}.</div><div style="text-align: center; font-weight: bold;">{{Turn}} To Move</div>[fen]{{Before}}[/fen]',
-                'afmt': '{{FrontSide}}<hr id="after">[fen]{{After}}[/fen]',
+                "name": "Card 1",
+                "qfmt": '<div style="text-align: center; font-weight: bold;">{{Move Number}}.</div><div style="text-align: center; font-weight: bold;">{{Turn}} To Move</div>[fen]{{Before}}[/fen]',
+                "afmt": '{{FrontSide}}<hr id="after">[fen]{{After}}[/fen]',
             },
-        ]
+        ],
     )
-    
+
     # Create a new Anki deck
     deck = genanki.Deck(model_id, name)
 
@@ -98,16 +101,15 @@ def main(source_pgn, color_pick):
 
     # Iterate through the moves
     traveler = pgn_travel(pgn)
-    if color_pick != '':
-        selected_moves = (step for step in traveler if step[3]==color_pick)
+    if color_pick != "":
+        selected_moves = (step for step in traveler if step[3] == color_pick)
     else:
         selected_moves = (step for step in traveler)
-        
 
     for move in selected_moves:
         board, following_board, play, turn = move
-        move_counter = int(np.ceil((play+1)/2))
-        
+        move_counter = int(np.ceil((play + 1) / 2))
+
         # turn = board.turn
         # if turn == chess.WHITE:
         #     move_counter += 1
@@ -116,59 +118,55 @@ def main(source_pgn, color_pick):
         #     turn = 'Black'
 
         # current_board = board.fen()
-        
+
         # Make the move on the board
-        # board.push(move)        
+        # board.push(move)
         # following_board = board.fen()
 
         # Create a new Anki note with the board in FEN notation on the front
         note = genanki.Note(
             model=anki_model,
-            fields=
-            [board.fen(), 
-            following_board.fen(), 
-            str(move_counter),
-            turn
-            ]
+            fields=[board.fen(), following_board.fen(), str(move_counter), turn],
         )
         deck.add_note(note)
-        
+
     # Save the deck to a file
     try:
         file_name = name.replace(" vs ", "_vs_").replace(" ", "")
         print(file_name)
-        genanki.Package(deck).write_to_file(file_name+'.apkg')
+        genanki.Package(deck).write_to_file(file_name + ".apkg")
     except:
-        file_name = 'ankipgn'
+        file_name = "ankipgn"
         print(file_name)
-        genanki.Package(deck).write_to_file(file_name+'.apkg')
-        
-        
+        genanki.Package(deck).write_to_file(file_name + ".apkg")
 
-if __name__ == "__main__":
-    # Parse the arguments
-    args = parseArguments()
 
+def main(args):
     # Raw print arguments
     print("You are running the script with arguments: ")
     for a in args.__dict__:
         print(str(a) + ": " + str(args.__dict__[a]))
-    
+
     game_source = args.source
     color_pick = args.color
-    
+
     if game_source[-4:] == ".pgn":
-        source_pgn = open(game_source)        
+        source_pgn = open(game_source)
     elif game_source.find("chessgames.com") >= 0:
         pgn_string = fetch_pgn(game_source)
         source_pgn = io.StringIO(pgn_string)
-    else: 
+    else:
         print("Source doesn't seem to be neither PGN file nor chessgames game link")
         sys.exit()
 
-    if color_pick and color_pick not in ['White', 'Black']:
+    if color_pick and color_pick not in ["White", "Black"]:
         print("Color pick can be either 'White' or 'Black'")
         sys.exit()
-        
-    main(source_pgn, color_pick)
 
+    convert(source_pgn, color_pick)
+
+
+if __name__ == "__main__":
+    # Parse the arguments
+    args = parseArguments()
+    main(args)
